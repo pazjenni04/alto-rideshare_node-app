ce04c4c98685537b015b97839384f537718901c7
Jennifer Davila <pazjenni1331@gmail.com>
2021-11-01 21:21:17 -0500

    need to see why won't render driver by id.

--- data/driver.js
+++ data/driver.js
@@ -6,8 +6,8 @@ const drivers = [
       "Steph is a graduate of SMU in Dallas, TX and fluent in Portugeuse, Spanish, and English. Steph has been driving with Alto since 2018.",
     phone: "214-546-0000",
     vehicle: "Alto 09",
-    "vehicle-make": "2019 Volkswagon Atlas",
-    "vehicle-color": "white",
+    vehicleMake: "2019 Volkswagon Atlas",
+    vehicleColor: "white",
   },
   {
     id: 2,
@@ -16,8 +16,8 @@ const drivers = [
       "Michael has been working for Alto since 2020.  He is currently undergoing a MBA degree at the Texas Woman's University.",
     phone: "214-000-8549",
     vehicle: "Alto 08",
-    "vehicle-make": "2019 Volkswagon Atlas",
-    "vehicle-color": "white",
+    vehicleMake: "2019 Volkswagon Atlas",
+    vehicleColor: "white",
   },
   {
     id: 3,
@@ -26,8 +26,8 @@ const drivers = [
       "Tommy adament volunteer at the North Texas food bank.  Outside of driving for Alto, Tommy represents Alto actively in the community by volunteering at shelters and food pantries.",
     phone: "214-000-8549",
     vehicle: "Alto 07",
-    "vehicle-make": "2019 Volkswagon Atlas",
-    "vehicle-color": "white",
+    vehicleMake: "2019 Volkswagon Atlas",
+    vehicleColor: "white",
   },
 ];
 
--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -4,7 +4,22 @@ const driverData = require("../data/driver");
 
 module.exports = (app) => {
   //fetches all drivers available
+  app.get("/api/drivers/:id", (req, res) => {
+    const chosen = req.params.id;
+
+    for (let i = 0; i < driverData.length; i++) {
+      const currentDriver = driverData.drivers.id[i];
+      if (chosen === currentDriver.id) {
+        return res.json(currentDriver[i]);
+      }
+    }
+
+    return res.json(false);
+  });
+
+  //gets all drivers
   app.get("/api/drivers", (req, res) => {
     res.json(driverData);
+    console.log(driverData);
   });
 };
