f21369ae0768028da66043102aa41c89c7329299
Jennifer Davila <pazjenni1331@gmail.com>
2021-11-09 21:39:43 -0600

    cleaned up code

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "images")));
 
 require("./routes/driverRoutes")(app);
 require("./routes/userRoutes")(app);
